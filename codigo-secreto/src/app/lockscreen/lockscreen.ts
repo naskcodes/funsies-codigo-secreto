92601776e7153bc19de1e9438ae91f5f654be3d8
Roberta Nascimento <60294500+RNask@users.noreply.github.com>
2025-07-10 09:44:44 -0300

    feat: adiçao de um botao de retorno para a pagina anterior

--- codigo-secreto/src/app/lockscreen/lockscreen.ts
+++ codigo-secreto/src/app/lockscreen/lockscreen.ts
@@ -19,10 +19,17 @@ export class Lockscreen {
       document.querySelector('.lockscreen-container')!.innerHTML = `
       <div class="lockscreen-container-resultado">
         <p class="lockscreen-container-resultado-p">Você digitou o número ${this.codenumber.value}</p>
+        <button class="lockscreen-container-resultado_button" type="button" (click)="retornaPaginaInicial()">Retornar</button>
       </div>
-      `;   
-    } else {
+      `;
+      document.querySelector('.lockscreen-container-resultado_button')?.addEventListener('click', bind(this))
+    } else if (this.codenumber.value = "47") {
       this.router.navigate(["/hitman"]);
-    };
+    }
   };
+
+  retornaPaginaInicial() {
+    console.log("clicou");
+    this.router.navigate(["/lockscreen"]);
+  }
 };
